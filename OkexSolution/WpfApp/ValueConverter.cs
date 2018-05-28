9fbb171c4e67bb4d0c4d8720cc25776f803aaa2c
sixteen <yinyazuolun@sina.com>
2018-05-28 08:51:24 +0800

    完成列头,下一步是大于1 显示红色

---
+++ OkexSolution/WpfApp/ValueConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace WpfApp
+{
+    public class ValueConverter : DependencyObject, IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (null != value)
+            {
+                if (int.TryParse(value.ToString(), out int number))
+                    return number > 1;
+            }
+            return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return 1;
+        }
+    }
+}
